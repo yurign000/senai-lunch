3e3ad0fe604304d5db7897d4e8de5a5631574611
yurign000 <yurign000@gmail.com>
2023-07-04 16:49:15 -0300

    feat: add context working with react router

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,7 +1,29 @@
 import { Outlet } from "react-router-dom";
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+
+import MainPage from './pages/mainPage/MainPage';
+import ProductList from './pages/ProductList/ProductList'
+
+import { useContext } from "react";
+import { UsuarioContext } from "./context/UsuarioContext";
 
 export default function App(){
+    const {logado} = useContext(UsuarioContext);
+
+    const router = createBrowserRouter([
+        {
+            path: "/", 
+            element: <MainPage/>
+        },
+        {
+            path: "/product-list", 
+            element: logado ? <ProductList/> : <MainPage/>
+        },
+    ]);
+    
     return (
-        <Outlet/>
+        <>
+            <RouterProvider router={router} />
+        </>
     );
 }
--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,28 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MainPage from './pages/mainPage/MainPage';
-import ProductList from './pages/ProductList/ProductList'
 import App from './App';
 import './index.css'
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children: [
-      {path: "/", element: <MainPage/>},
-      {path: "/product-list", element: <ProductList/>},
-    ]
-  }
-]);
-
 import { UsuarioProvider } from './context/UsuarioContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
     <UsuarioProvider>
-      <RouterProvider router={router} />
+      <App/>
     </UsuarioProvider>
   </React.Fragment>
 )
